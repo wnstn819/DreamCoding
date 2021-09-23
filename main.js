d2a0d47e559b832f50a5d3e69756606053a4ee6c
왕준수 <wnstn819@naver.com>
2021-09-23 10:08:24 +0900

    fade_JS

--- main.js
+++ main.js
@@ -29,6 +29,39 @@ contactBtn.addEventListener("click", () => {
   scrollIntoView("#contact");
 });
 
+// Scrolling fade out
+
+const home = document.querySelector(".home__container");
+const homeHeight = home.getBoundingClientRect().height;
+
+const about = document.querySelector(".about__container");
+const aboutHeight = about.getBoundingClientRect().height;
+
+const skill = document.querySelector(".skill__container");
+const skillHeight = skill.getBoundingClientRect().height;
+
+const work = document.querySelector(".work__container");
+const workHeight = work.getBoundingClientRect().height;
+
+const testimonials = document.querySelector(".testimonials__container");
+const testimonialsHeight = testimonials.getBoundingClientRect().height;
+
+document.addEventListener("scroll", () => {
+  home.style.opacity = 1 - window.scrollY / homeHeight;
+  about.style.opacity = 1.4 - window.scrollY / (homeHeight + aboutHeight);
+  skill.style.opacity =
+    1.5 - window.scrollY / (homeHeight + aboutHeight + skillHeight);
+  work.style.opacity =
+    1.6 -
+    window.scrollY / (homeHeight + aboutHeight + skillHeight + workHeight);
+  console.log(workHeight);
+
+  testimonials.style.opacity =
+    1.3 -
+    window.scrollY / (homeHeight + aboutHeight + skillHeight + workHeight) +
+    testimonialsHeight;
+});
+
 function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
   scrollTo.scrollIntoView({ behavior: "smooth" });
